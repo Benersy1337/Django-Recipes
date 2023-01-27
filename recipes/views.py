e27ab196c1c42c6f90bf03d74c3a2aebf7118528
Benersy1337 <benersy1337@gmail.com>
2023-01-27 13:14:53 -0300

    Header and Input-Search Done

---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luan Roberto',
+    })
