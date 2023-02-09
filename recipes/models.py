cd847b91ff90767d9da65e5a99bdd78dcc8684cc
Benersy1337 <benersy1337@gmail.com>
2023-02-09 13:59:52 -0300

    create view category, change fake data of file to models and http404 if not recipe

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    
+    # Ao invés de "Category object (1)" vai ser agora -> "Carnes", por exemplo, quando for criado no /admin
+    def __str__(self) -> str:
+        return self.name
 
 class Recipe(models.Model):
     
@@ -17,14 +21,17 @@ class Recipe(models.Model):
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
+    # Atualiza a data apenas na criação do objeto (auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    # Atualiza a data toda vez que o objeto é modificiado (auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     
-    
+    def __str__(self) -> str:
+        return self.title
     
     
 
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from recipes.views import home, recipe
+from . import views
+from django.conf.urls.static import static
+
+from django.conf import settings
+
 
 urlpatterns = [
-    path('', home, name="recipes-home"),
-    path('recipes/<int:id>/', recipe, name="recipes-recipe"),   
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),   
     # path('recipes/<uuid:id>/', recipe),    
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
